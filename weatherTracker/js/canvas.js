a6e453eb16a48a4da00b7376cb2b1c26060e9339
BSelyomin <bogdan.selyomin@gmail.com>
2022-12-19 13:41:46 -0500

    12/19

--- weatherTracker/js/canvas.js
+++ weatherTracker/js/canvas.js
@@ -13,7 +13,7 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 let background = new Image();
-background.src = "../equirectanglular.png";
+background.src = "/equirectanglular.png";
 
 // Make sure the image is loaded first otherwise nothing will draw.
 background.onload = function () {
